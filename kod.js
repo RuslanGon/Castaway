16126415ecd5fc8a841c778220475a946743ce95
RuslanGon <ruslan441006@gmail.com>
2023-12-28 11:18:07 +0200

    edwd

--- kod.js
+++ kod.js
@@ -1,1 +1,9 @@
-// console.log(document);
+const elem = document.querySelector(`.header-list`);
+console.log(elem);
+
+const elem1 = document.querySelector(`.header-item`);
+console.log(elem1);
+// elem1.textContent = `Hello`;
+
+const elem2 = document.querySelector(`.header-item:nth-child(2)`);
+console.log(elem2)
