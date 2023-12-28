638f367fe2e323258a957543251d5feda7fa8f47
RuslanGon <ruslan441006@gmail.com>
2023-12-28 11:44:58 +0200

    dww

--- kod.js
+++ kod.js
@@ -1,9 +1,28 @@
-const elem = document.querySelector(`.header-list`);
-console.log(elem);
+// const elem = document.querySelector(`.header-list`);
+// console.log(elem);
 
-const elem1 = document.querySelector(`.header-item`);
-console.log(elem1);
-// elem1.textContent = `Hello`;
+// const elem1 = document.querySelector(`.header-item`);
+// console.log(elem1);
+// // elem1.textContent = `Hello`;
 
-const elem2 = document.querySelector(`.header-item:nth-child(2)`);
-console.log(elem2)
+// const elem2 = document.querySelector(`.header-item:nth-child(2)`);
+// console.log(elem2);
+
+// const elem4 = elem.querySelector(`.header-item:nth-child(4)`);
+// console.log(elem4);
+
+
+
+// const elemNav = document.querySelector(`.header-nav`);
+// console.log(elemNav)
+
+// const liElems = elemNav.querySelectorAll(`.header-item`);
+// console.log(liElems);
+
+// console.log(Array.from(liElems));
+
+// console.log([...liElems])
+
+// console.log(liElems[2]);
+
+
